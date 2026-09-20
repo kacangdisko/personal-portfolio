18a8e486ad5be151f3c3b3be89b0a12457450c49
kacangdisko <rizhadzaky@gmail.com>
2026-09-21 01:36:38 +0700

    Mobile update

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -28,7 +28,7 @@ const mobileSections = dockOrder.filter((key) => key !== "help");
 export default function MobileNav({ onSelect }: Props) {
   return (
     <div className="mobile-sections">
-      <div className="eyebrow">Figure out more!</div>
+      <div className="eyebrow">Find out more!</div>
       <div className="mobile-sections-list">
         {mobileSections.map((key) => (
           <button
